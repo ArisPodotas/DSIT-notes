061d755b913aab92ce6e5c2f1734dd98153eff90
Aris Podotas <a.Podotas@gmail.com>
2024-12-24 13:48:54 +0200

    Clustering project when i removed the corcoeff and added the pca while finishing the images of the histograms

--- Semester A/Clustering Algorithms/Clustering_Project_2024_2025/main.m
+++ Semester A/Clustering Algorithms/Clustering_Project_2024_2025/main.m
@@ -1,3 +1,4 @@
+ 
 % This is the file that implements the changes made for the project
 % By Aris Podotas for DSIT , Clustering Algorithms.
 
@@ -21,11 +22,15 @@ x = struct('labels', load('Labels_Salinas.mat').Labelsinit, 'data', load('Salina
 % In this way we re-phrase our problem to one that has
 % M times N data vectors of length L featrues.
 
+% Feeling the data
+
+% Missing data
+
 % Removing vectors with zeros in the labels
 function output = filter(object)
     % Generating a matrix of boolians that has a true value
     % for all non-missing labels
-    missing = find(object.labels~=0);
+    missing = object.labels~=0;
     % Using the matrix of boolians to remove false values
     output = struct('labels', object.labels(missing), 'data', object.data(missing));
 end
@@ -49,12 +54,13 @@ evaluate(x);
 filtered = filter(x);
 % Thus concludes the missing data
 
-% Feeling the data
+% Data type
+
 function values(object)
-    object.stats= [mean(object.data);
-    max(object.data);
-    min(object.data);
-    std(object.data);];
+    object.stats= [mean(object.data(object.labels ~= 0));
+    max(object.data(object.labels ~= 0));
+    min(object.data(object.labels ~= 0));
+    std(object.data(object.labels ~= 0));];
 end
 
 values(x);
@@ -62,25 +68,52 @@ values(x);
 % Visualizing the distributions of the data
 % 204 histograms seem a little excesive for inside
 % the report so enable this optionally.
-function visualize(object)
-    [~, ~, L] = size(object.data);
-    % Using Struge's rule
-    optimal = ceil(log2(L)+1);
-    figure(1)
-    histogram(object.data(:,:,1), optimal)
+function visualize(object, autoclose, save)
+    [M, N, L] = size(object.data);
+    % Removing zero labels
+    c = sum(sum(object.labels == 0)); % 8592
+    % Using Sturge's rule
+    % Writing this made me realise I had used the rule wrong last time
+    optimal = ceil(log2(M*N-c)+1);
+    mkdir('./Images')
+    for i=1:L
+        fig = figure(i);
+        % Not sure why but the syntax
+        % histogram(object.data(object.labels ~=0, i)) wont work
+        holder = object.data(:,:,i);
+        histogram(holder(object.labels ~= 0), optimal)
+        title(sprintf('Feature %.0f histogram', i))
+        if save
+            saveas(i, sprintf('./Images/Feature %.0f histogram.png', i))
+        end
+        if autoclose
+            close (fig);
+        end
+    end
 end
 
-visualize(x);
+% Change autoclose and save to your preference.
+% They only need to be on the first time.
+visualize(x, 1, 0);
 
-% Cross correlation
-function compare(object)
-    object.relations = corrcoef(object.data);
+% Feature selection
+
+% Principal componenet analysis from the provided file
+
+% Determining m
+
+pca_fun(x)
+
+% Feature transformations
+
+function transform(object)
+
 end
 
-compare(x);
+% Clustering
 
-% Implementing a function to generate for theta values
-function theta = generate(sheet, number)
+% Implementing a function to generate theta values
+function theta = generate(object, number)
     % This is going to need to return values within the convex hull
     % Example theta that could be output
     % theta = [0, 20, 0, 20;
@@ -92,12 +125,10 @@ function theta = generate(sheet, number)
     %          0, 0, 20, 20;
     %          0, 0, 20, 20;
     %          0, 0, 20, 20;];
-    [~,N] = size(sheet.Countrydata);
-    data = [max(sheet.Countrydata);
-            min(sheet.Countrydata);];
-    theta = ones(number,N);
+    [~,~,L] = size(object.data);
+    theta = ones(number, L);
     for i=1:number
-        for j=1:N
+        for j=1:L
             % Normalized random initial value in the range [min, max]
             theta(i,j) = (max(data(1)))*(randn()) + min(data(2));
         end
@@ -106,7 +137,7 @@ end
 
 % Implementing a function to find the number of clusters
 function amount(sheet, seed)
-    figure(1000 + seed)
+    figure(seed)
     costs = ones(1,10);
     for j = 2:10
         theta = generate(sheet, j);
@@ -118,22 +149,10 @@ function amount(sheet, seed)
     ylabel('Cost function')
 end
 
-% Number of clusters
-% amount(x, 4);
-% All the below are problematic figures mentioned in the report
-% amount(xcopy, 62);
-% amount(normalization, 102);
-% amount(ncopy, 1038);
-
 function cluster(sheet, clusters)
-    % Clusters refers to the number of clusters
-    % Implementing a function to do each iteration of the clustering for each alrgorithm so that the iterations throught the data is simple
-    % Remember that the previous final figure was figure 90
     [l, N] = size(sheet.Countrydata); % Keep in mind that this N is variable
-    % Looping to try different clusterings
     theta = generate(sheet, clusters);
     % k-means
-    % Note we need to transpose since our data vectors are on rows
     [theta,bel,J] = k_means(sheet.Countrydata', theta');
     [N, m] = size(theta);
     for i=1:(N/3)
@@ -161,111 +180,6 @@ function cluster(sheet, clusters)
             zlabel('z')
         else
             title('k-means final 3 features')
-            xlabel('x')
-            ylabel('y')
-            zlabel('z')
-        end
-    end
-
-    close all
-    % k-medoids
-    [bel,cost,theta,a] = k_medoids(sheet.Countrydata', clusters, 72);
-    [N, m] = size(theta);
-    for i=1:(N/3)
-        figure(90 + i + N/3), hold on
-        figure(90 + i + N/3), grid on
-        for j=1:m
-            figure(90 + i + N/3), plot3(theta(i,j), theta(i + 1,j), theta(i + 2,j), 'k+')
-        end
-        figure(90 + i + N/3), plot3(sheet.Countrydata(bel==1,i), sheet.Countrydata(bel==1, i+ 1), sheet.Countrydata(bel==1,i+2), 'ro')
-        figure(90 + i + N/3), plot3(sheet.Countrydata(bel==2,i), sheet.Countrydata(bel==2, i+ 1), sheet.Countrydata(bel==2,i+2), 'g*')
-        figure(90 + i + N/3), plot3(sheet.Countrydata(bel==3,i), sheet.Countrydata(bel==3, i+ 1), sheet.Countrydata(bel==3,i+2), 'b.')
-        figure(90 + i + N/3), plot3(sheet.Countrydata(bel==4,i), sheet.Countrydata(bel==4, i+ 1), sheet.Countrydata(bel==4,i+2), 'ys')
-        for k = 1:l
-            text(sheet.Countrydata(k,i),sheet.Countrydata(k,i+1) ,sheet.Countrydata(k,i+2) , sheet.country(k), 'FontSize', 6, 'HorizontalAlignment', 'left')
-        end
-        if i == 1
-            title('k-medoids first 3 features')
-            xlabel('x')
-            ylabel('y')
-            zlabel('z')
-        elseif i==2
-            title('k-medoids second 3 features')
-            xlabel('x')
-            ylabel('y')
-            zlabel('z')
-        else
-            title('k-medoids final 3 features')
-            xlabel('x')
-            ylabel('y')
-            zlabel('z')
-        end
-    end
-
-    % k-medians
-    theta = generate(sheet, clusters);
-    [theta,bel,J] = k_medians(sheet.Countrydata', theta');
-    [N, m] = size(theta);
-    for i=1:(N/3)
-        figure(90 + i + 2*N/3), hold on
-        figure(90 + i + 2*N/3), grid on
-        for j=1:m
-            figure(90 + i + 2*N/3), plot3(theta(i,j), theta(i + 1,j), theta(i + 2,j), 'k+')
-        end
-        figure(90 + i + 2*N/3), plot3(sheet.Countrydata(bel==1,i), sheet.Countrydata(bel==1, i+ 1), sheet.Countrydata(bel==1,i+2), 'ro')
-        figure(90 + i + 2*N/3), plot3(sheet.Countrydata(bel==2,i), sheet.Countrydata(bel==2, i+ 1), sheet.Countrydata(bel==2,i+2), 'g*')
-        figure(90 + i + 2*N/3), plot3(sheet.Countrydata(bel==3,i), sheet.Countrydata(bel==3, i+ 1), sheet.Countrydata(bel==3,i+2), 'b.')
-        figure(90 + i + 2*N/3), plot3(sheet.Countrydata(bel==4,i), sheet.Countrydata(bel==4, i+ 1), sheet.Countrydata(bel==4,i+2), 'ys')
-        for k = 1:l
-            text(sheet.Countrydata(k,i),sheet.Countrydata(k,i+1) ,sheet.Countrydata(k,i+2) , sheet.country(k), 'FontSize', 8, 'HorizontalAlignment', 'left')
-        end
-        if i == 1
-            title('k-medians first 3 features')
-            xlabel('x')
-            ylabel('y')
-            zlabel('z')
-        elseif i==2
-            title('k-medians second 3 features')
-            xlabel('x')
-            ylabel('y')
-            zlabel('z')
-        else
-            title('k-medians final 3 features')
-            xlabel('x')
-            ylabel('y')
-            zlabel('z')
-        end
-    end
-
-    % Possibilistic
-    eta = ones(1, clusters);
-    [U, theta] = possibi(sheet.Countrydata', clusters, eta,1,73,1,0.01);
-    [N, m] = size(theta);
-    for i=1:(N/3)
-        figure(90 + i + 3*N/3), hold on
-        figure(90 + i + 3*N/3), grid on
-        for j=1:m
-            figure(90 + i + 3*N/3), plot3(theta(i,j), theta(i + 1,j), theta(i + 2,j), 'k+')
-        end
-        figure(90 + i + 3*N/3), plot3(sheet.Countrydata(bel==1,i), sheet.Countrydata(bel==1, i+ 1), sheet.Countrydata(bel==1,i+2), 'ro')
-        figure(90 + i + 3*N/3), plot3(sheet.Countrydata(bel==2,i), sheet.Countrydata(bel==2, i+ 1), sheet.Countrydata(bel==2,i+2), 'g*')
-        figure(90 + i + 3*N/3), plot3(sheet.Countrydata(bel==3,i), sheet.Countrydata(bel==3, i+ 1), sheet.Countrydata(bel==3,i+2), 'b.')
-        figure(90 + i + 3*N/3), plot3(sheet.Countrydata(bel==4,i), sheet.Countrydata(bel==4, i+ 1), sheet.Countrydata(bel==4,i+2), 'ys')
-        for k = 1:l
-            text(sheet.Countrydata(k,i),sheet.Countrydata(k,i+1) ,sheet.Countrydata(k,i+2) , sheet.country(k), 'FontSize', 8, 'HorizontalAlignment', 'left')
-        end
-        if i == 1
-            title('Possibilistic c-means first 3 features')
-            xlabel('x')
-            ylabel('y')
-            zlabel('z')
-        elseif i==2
-            title('Possibilistic c-means second 3 features')
-            xlabel('x')
-            ylabel('y')
-            zlabel('z')
-        else
-            title('Possibilistic clustering algorithm final 3 features')
             xlabel('x')
             ylabel('y')
             zlabel('z')
