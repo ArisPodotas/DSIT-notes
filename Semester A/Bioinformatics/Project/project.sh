f042fd58b06f2353f72bdaecc5f3f02b98bcddd4
Aris Podotas <a.Podotas@gmail.com>
2025-01-15 13:09:16 +0200

    Gonna do bioinf on the vm

--- Semester A/Bioinformatics/Project/project.sh
+++ Semester A/Bioinformatics/Project/project.sh
@@ -1,1 +1,31 @@
-# Chip seq piopleine
+#!/bin/bash
+
+# Chip seq pipleine
+
+# Remember that the bowtie-build has been done manually prior to this
+
+# Paths
+mkdir $1
+mkdir $2
+
+# Isolating .sra files
+files = ls *.sra
+
+# Making fastq
+for i in "${files[@]}"; do
+    fastq-dump i -O ./
+done
+
+# Using bowtie
+
+# Indexing
+
+# Converting to bam
+
+# Bedtools
+
+# BigWig
+
+# Using macs
+
+# Finding peaks
