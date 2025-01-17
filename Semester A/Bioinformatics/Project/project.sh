cf7a9bddb93d8ea752cb88cd47242b12231d85af
Aris Podotas <a.Podotas@gmail.com>
2025-01-17 15:07:57 +0200

    Bioinfo project files sent to ubuntu

--- Semester A/Bioinformatics/Project/project.sh
+++ Semester A/Bioinformatics/Project/project.sh
@@ -2,23 +2,38 @@
 
 # Chip seq pipleine
 
-# Remember that the bowtie-build has been done manually prior to this
+# Installing the sra toolkit
+# wget --output-document sratoolkit.tar.gz https://ftp-trace.ncbi.nlm.nih.gov/sra/sdk/current/sratoolkit.current-ubuntu64.tar.gz
+# tar -vxzf sratoolkit.tar.gz
+
+# Downloading files
+# prefetch SRR452931
+# mv ./SRR452931/SRR452931.sra ./
+# rm ./SRR452931/
+# fastq-dump -v --split-3 SRR452931
+# prefetch SRR524934
+# mv ./SRR524934/SRR524934.sra ./
+# rm ./SRR524934/
+# fastq-dump -v --split-3 SRR524934
+# prefetch SRR524936
+# mv ./SRR524936/SRR524936.sra ./
+# rm ./SRR524936/
+# fastq-dump -v --split-3 SRR524936
+# prefetch SRR524939
+# mv ./SRR524939/SRR524939.sra ./
+# rm ./SRR524939/
+# fastq-dump -v --split-3 SRR524939
+
+# Getting the genome
+# wget -a https://hgdownload.cse.ucsc.edu/goldenpath/hg18/bigZips/hg18.chrom.sizes
+# wget -a https://hgdownload.cse.ucsc.edu/goldenpath/hg18/bigZips/hg18.fa.gz
+#gzip -d hg18.fa.gz
+
+# bowtie-build hg18.fa hg18
 
 # Paths
 mkdir $1
 mkdir $2
-
-# Isolating .sra files
-files = ls *.sra
-
-# Making fastq
-for i in "${files[@]}"; do
-    fastq-dump i -O ./
-done
-
-# Using bowtie
-
-# Indexing
 
 # Converting to bam
 
